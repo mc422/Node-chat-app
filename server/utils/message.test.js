fa717d2c7d84386d1e96c313aaffc9b3d1f1f668
chaomeng <chaomeng04@gmail.com>
2018-06-11 23:50:06 -0700

    add generateLocationMessage test

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message', () => {
@@ -11,6 +11,22 @@ describe('generateMessage', () => {
     expect(message).toMatchObject({
       from,
       text
+    });
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should genereate correct location message', () => {
+    var from = 'user123';
+    var latitude = '123';
+    var longitude = '156';
+    var url = `https://www.google.com/maps?q=${latitude},${longitude}`
+
+    var message = generateLocationMessage(from, latitude, longitude);
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({
+      from,
+      url
     });
   });
 });
